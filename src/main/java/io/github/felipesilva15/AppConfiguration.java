c320bc7243f074f012c195f44aedb72bc1a0c1fd
Felipe Silva <felipe.allware@gmail.com>
2023-07-06 20:50:10 -0300

    Limpando o projeto de começando com a conexão com o BD

--- src/main/java/io/github/felipesilva15/AppConfiguration.java
+++
@@ -1,8 +0,0 @@
-package io.github.felipesilva15;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class AppConfiguration {
-}
--- src/main/java/io/github/felipesilva15/VendasApplication.java
+++ src/main/java/io/github/felipesilva15/VendasApplication.java
@@ -1,12 +1,7 @@
 package io.github.felipesilva15;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -14,13 +9,5 @@ import org.springframework.web.bind.annotation.RestController;
 public class VendasApplication {
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);
-    }
-
-    @Value("${application.name}")
-    public String applicationName;
-
-    @GetMapping("/hello")
-    public String hello() {
-        return applicationName;
     }
 }
