50c92fd855f9e130b135b5f440b6880a3964b60f
Felipe Silva <felipe.allware@gmail.com>
2023-07-13 00:29:55 -0300

    Trocando a base de dados o projeto para MySQL

--- src/main/java/io/github/felipesilva15/domain/entity/ItemPedido.java
+++ src/main/java/io/github/felipesilva15/domain/entity/ItemPedido.java
@@ -15,7 +15,7 @@ import java.math.BigDecimal;
 @Table(name = "item_pedido")
 public class ItemPedido {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Integer id;
 
